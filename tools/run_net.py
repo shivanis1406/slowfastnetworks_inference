5849d9675b55c9ebfeb5a31dfc30d5ba08c968c8
shivanis14 <shivanis1406@gmail.com>
2024-10-15 11:10:34 +0000

    first commit

--- tools/run_net.py
+++ tools/run_net.py
@@ -5,11 +5,17 @@
 from slowfast.config.defaults import assert_and_infer_cfg
 from slowfast.utils.misc import launch_job
 from slowfast.utils.parser import load_config, parse_args
+'''
 from vision.fair.slowfast.tools.demo_net import demo
 from vision.fair.slowfast.tools.test_net import test
 from vision.fair.slowfast.tools.train_net import train
 from vision.fair.slowfast.tools.visualization import visualize
-
+'''
+#https://github.com/facebookresearch/SlowFast/issues/718#issuecomment-2356125849
+from demo_net import demo
+from test_net import test
+from train_net import train
+from visualization import visualize
 
 def main():
     """
